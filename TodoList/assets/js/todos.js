fe7e90bf4f4768b951955dc72508de6d69a7ff49
Nathan <nathangisvold@gmail.com>
2017-03-14 16:10:37 -0700

    toggle completed css class

--- TodoList/assets/js/todos.js
+++ TodoList/assets/js/todos.js
@@ -1,20 +1,4 @@
 // Check off specific todo by clicking
 $("li").click(function(){
-	//if li is gray
-	if($(this).css("color") === "rgb(128, 128, 128)"){
-		//turn black
-		$(this).css({
-			color: "black",
-			textDecoration:"none"
-		});
-	}
-	//else
-	else {
-		//turn gray	
-		$(this).css({
-			color: "gray",
-			textDecoration:"line-through"
-		});
-	}
-
+	$(this).toggleClass("completed")
 });
