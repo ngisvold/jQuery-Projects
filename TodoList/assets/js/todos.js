bd87b35b85c43c8730329412e4b097cb0e63b5d0
Nathan <nathangisvold@gmail.com>
2017-03-14 16:47:49 -0700

    Change to On Event

--- TodoList/assets/js/todos.js
+++ TodoList/assets/js/todos.js
@@ -1,12 +1,23 @@
 // Check off specific todo by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("completed");
 });
 
 //click on X to delete Todo
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//get new todo from input
+		var todoText = $(this).val();
+		//clear input
+		$(this).val("");
+		//create new li to add to ul
+		$("ul").append("<li><span>X</span> "+ todoText +"</li>");
+	}
 });
